c57f271c5d3de11a19d5701a3091796645ae5194
Cheng <david2881234@gmail.com>
2020-07-14 00:20:03 +0800

    finish part4

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['176.34.38.159', 'django.chuishen.myinstance.com', 'localhost',
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
